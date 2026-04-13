5a2e3fa748729366d1c88cfe71a044527a8d94ec
AquinoPL <pedroluisag100@gmail.com>
2026-04-13 14:26:04 -0500

    Primer commit

--- config/database.php
+++ config/database.php
@@ -1,9 +1,20 @@
 <?php
+
+$host = "localhost";
+$port = "5432";
+$dbname = "db_support";
+$user = "postgres";
+$password = "123456789";
+
+/*
 $host = "aws-1-us-east-1.pooler.supabase.com";
 $port = "6543";
 $dbname = "postgres";
 $user = "postgres.wxanqkmfafgcmbwroxzg";
 $password = "soporte*2026";
+*/
+
+
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
